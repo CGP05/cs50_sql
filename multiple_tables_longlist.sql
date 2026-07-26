ba0226e8dc118f8a1d6b06466417bc2033799bab
Christian <christian_pasquali@yahoo.com>
2026-07-26 09:39:52 -0400

    double nested query

--- multiple_tables_longlist.sql
+++ multiple_tables_longlist.sql
@@ -1,1 +1,6 @@
-SELECT "isbn" FROM "longlist" WHERE "title" = 'The Birthday Party';
+SELECT "author" FROM "longlist" WHERE "isbn" = (
+	SELECT "author" FROM "longlist" WHERE "isbn" = (
+		SELECT "isbn" FROM "longlist"
+		WHERE "title" = 'The Birthday Party'
+	)
+);
