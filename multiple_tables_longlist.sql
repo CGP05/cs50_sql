b8d3b3bdeff4dc9e5ef989f92e6553c1689e878d
Christian <christian_pasquali@yahoo.com>
2026-07-27 11:01:57 -0400

    Fernanda

--- multiple_tables_longlist.sql
+++ multiple_tables_longlist.sql
@@ -1,6 +1,1 @@
-SELECT "author" FROM "longlist" WHERE "isbn" = (
-	SELECT "author" FROM "longlist" WHERE "isbn" = (
-		SELECT "isbn" FROM "longlist"
-		WHERE "title" = 'The Birthday Party'
-	)
-);
+SELECT "isbn" FROM "longlist" WHERE "name" = 'Fernanda Melchor'
