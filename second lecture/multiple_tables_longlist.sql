1be6e1e1d973c11af1b17e9e4150ea7d07ca7a39
Christian <christian_pasquali@yahoo.com>
2026-07-27 13:10:02 -0400

    double nested query for Fernanda Melchor book titles

--- second lecture/multiple_tables_longlist.sql
+++ second lecture/multiple_tables_longlist.sql
@@ -1,3 +1,7 @@
 SELECT "title" FROM "books" WHERE "id" IN (
 	SELECT "book_id" FROM "authored"
-	WHERE "
+	WHERE "author_id" = (
+		SELECT "id" FROM "authors" WHERE "name" = 'Fernanda Melchor'
+		)
+	)
+	
