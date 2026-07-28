7def54dbee07b3dae5ed45e51fbb85ed66448e70
Christian <christian_pasquali@yahoo.com>
2026-07-28 19:16:44 -0400

    INTERSECT authors and translators

--- second lecture/sets_longlist.sql
+++ second lecture/sets_longlist.sql
@@ -1,3 +1,3 @@
-SELECT 'author' AS 'profession', "name" FROM "authors"
-UNION
-SELECT 'translator' AS 'profession', "name" FROM "translators";
+SELECT "name" FROM "authors"
+INTERSECT
+SELECT "name" FROM "translators";
