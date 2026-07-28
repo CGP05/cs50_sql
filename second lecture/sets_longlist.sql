cf5a5a38255f467d22c3607ccc5e4b601326d78c
Christian <christian_pasquali@yahoo.com>
2026-07-28 16:47:52 -0400

    column titled profession with every author and translator with their own title of their profession

--- second lecture/sets_longlist.sql
+++ second lecture/sets_longlist.sql
@@ -1,1 +1,3 @@
-SELECT 'author' AS 'profession', "name" FROM "authors";
+SELECT 'author' AS 'profession', "name" FROM "authors"
+UNION
+SELECT 'translator' AS 'profession', "name" FROM "translators";
