d3b3b65a15968fe0e1bb046cb7d686c83ba7bd15
Christian <christian_pasquali@yahoo.com>
2026-07-28 19:56:16 -0400

    book_id from translator Sophie Hughes

--- second lecture/sets_longlist.sql
+++ second lecture/sets_longlist.sql
@@ -1,3 +1,3 @@
-SELECT "name" FROM "authors"
-EXCEPT
-SELECT "name" FROM "translators";
+SELECT "book_id" from "translated" WHERE "translator_id" = (
+	SELECT "id" FROM "translators" WHERE "name" = 'Sophie Hughes'
+);
