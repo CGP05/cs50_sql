1f7e481b35dc8c7d604bc40d9b8f4f33ba15f90d
Christian <christian_pasquali@yahoo.com>
2026-07-29 14:30:42 -0400

    AVG rating

--- second lecture/sets_longlist.sql
+++ second lecture/sets_longlist.sql
@@ -1,7 +1,1 @@
-SELECT "book_id" from "translated" WHERE "translator_id" = (
-	SELECT "id" FROM "translators" WHERE "name" = 'Sophie Hughes'
-)
-INTERSECT
-SELECT "book_id" FROM "translated" WHERE "translator_id" = (
-	SELECT "id" FROM "translators" WHERE "name" = 'Margaret Jull Costa'
-);
+SELECT AVG("rating") FROM "ratings";
