60617ddda50fd826c01f7af957419cd1c16a9bac
Christian <christian_pasquali@yahoo.com>
2026-07-30 14:31:14 -0400

    # of ratings for each book

--- second lecture/sets_longlist.sql
+++ second lecture/sets_longlist.sql
@@ -1,4 +1,2 @@
-SELECT "book_id", ROUND(AVG("rating"), 2) AS "average rating"
-FROM "ratings"
-GROUP BY "book_id"
-HAVING "average rating" > 4.0;
+SELECT "book_id", COUNT("rating") FROM "ratings"
+GROUP BY "book_id";
