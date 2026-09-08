22d7f37388bc16b93d11bf94050028e787f63791
Christian Pasquali <christian_pasquali@yahoo.com>
2026-09-08 13:18:01 -0400

    WHERE collection_id table

--- week 3 - writing/update created table.sql
+++ week 3 - writing/update created table.sql
@@ -1,4 +1,8 @@
 UPDATE "created" SET "artist_id" = (
 	SELECT "id" FROM "artists"
 	WHERE "name" = "Li Yin"
+)
+WHERE "collection_id" = (
+	SELECT "id" FROM "collections"
+	WHERE "title" = 'Farmers working at dawn'
 );
